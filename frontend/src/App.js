5eda9bb1293434cc6f283c5b6caa658104a782f0
Om <omgajbhiye2@gmail.com>
2023-07-16 00:47:54 +0530

    contactPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Grievances from './components/Pages/Grievances';
+import contactUs from './components/Pages/contactUs';
 import Home from './components/Pages/Home';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact Component={Home} />
+        <Route path='/contact' exact Component={contactUs} />
         <Route path='/grievances' exact Component={Grievances} />
       </Routes>
     </Router>
